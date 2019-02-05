172f5ece6bf5704eebd9b6e2ba39f085f61a0d51
Federico <federicogerardi94@gmail.com>
2019-02-05 16:13:45 +0100

    Users database created and online users manager deployed; verified

--- turing-server/src/main/java/it/azraelsec/Server.java
+++ turing-server/src/main/java/it/azraelsec/Server.java
@@ -1,9 +1,42 @@
 package it.azraelsec;
 
-public class Server 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.net.SocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server
 {
+    public static final int DATA_BLOCK_SIZE = 512;
+    private static final int DEFAULT_TCP_PORT = 1337;
+    private static final int DEFAULT_UDP_PORT = 1338;
+    private static final int DEFAULT_RMI_PORT = 3400;
+    private static final String DEFAULT_DATA_DIR = "./";
+
+    private final ExecutorService TCPConnectionDispatcher;
+    public final UsersDB usersDB;
+    public final OnlineUsersDB onlineUsersDB;
+
+    public Server() {
+        TCPConnectionDispatcher = Executors.newCachedThreadPool();
+        onlineUsersDB = new OnlineUsersDB();
+        usersDB = new UsersDB();
+    }
+
+    /***
+     * Main method
+     */
     public static void main( String[] args )
     {
-        System.out.println( "Hello Server!" );
+        
     }
+
+    private void storeUserDB() {
+    }
+
+    /**
+     * HELPERS
+     */
+
 }
