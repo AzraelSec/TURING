fa18ad31a76fb40854d837323c2a0a03eb5ace55
Federico <federicogerardi94@gmail.com>
2019-02-09 11:08:51 +0100

    Client args parsing implemented

--- turing-client/src/main/java/it/azraelsec/Client.java
+++ turing-client/src/main/java/it/azraelsec/Client.java
@@ -2,22 +2,81 @@ package it.azraelsec;
 
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
 import net.sourceforge.argparse4j.inf.Subparsers;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Client 
 {
+    public static final int BLOCK_SIZE = 512;
+    private static int TCP_PORT = 1337;
+    private static int UDP_PORT = 1338;
+    private static int RMI_PORT = 3400;
+
+    public Client () {}
+
+    public void setup(Namespace cmdOptions) {
+        Optional.ofNullable(cmdOptions.getString("config_file")).ifPresent(this::loadConfig);
+        TCP_PORT = Optional.ofNullable( cmdOptions.getInt("tcp_command_port") ).orElseGet( () -> TCP_PORT );
+        UDP_PORT = Optional.ofNullable( cmdOptions.getInt("udp_port") ).orElseGet( () -> UDP_PORT );
+        RMI_PORT = Optional.ofNullable( cmdOptions.getInt("rmi_port") ).orElseGet( () -> RMI_PORT );
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("TURING Client is shutting down...");
+        }));
+        System.out.println(String.format("TCP_PORT: %s\nUDP_PORT: %s\nRMI_PORT: %s", TCP_PORT, UDP_PORT, RMI_PORT));
+    }
+    private void loadConfig(String filePath) {
+        if(checkConfigFile(filePath))
+        {
+            try{
+                JSONObject configs = new JSONObject(Files.lines(new File(filePath).toPath()).collect(Collectors.joining("\n")));
+                TCP_PORT = configs.has("TCP_PORT") ? configs.getInt("TCP_PORT") : TCP_PORT;
+                UDP_PORT = configs.has("UDP_PORT") ? configs.getInt("UDP_PORT") : UDP_PORT;
+                RMI_PORT = configs.has("RMI_PORT") ? configs.getInt("RMI_PORT") : RMI_PORT;
+            }
+            catch(Exception ex) {
+                System.out.println("JSON parsing error for file:" + filePath);
+                System.out.println("That's the reason:" + ex.getMessage());
+            }
+        }
+        else System.out.println("Configuration file not found");
+    }
+    private boolean checkConfigFile(String filePath) {
+        File configFile = new File(filePath);
+        return configFile.isFile() && configFile.exists();
+    }
+
+    /*
+    * MAIN METHOD
+    * */
+
     public static void main( String[] args )
     {
         ArgumentParser argpars = ArgumentParsers.newFor("TURING Client")
-            .build()
-            .defaultHelp(true)
-            .description("TURING distributed program client");
-        Subparsers commandParser = argpars.addSubparsers().title("command")
-            .help("Additional help").description("Valid commands").metavar("COMMAND");
-        commandParser.addParser("register").help("registra l'utente");
-        commandParser.addParser("login").help("effettua il login");
-        commandParser.addParser("logout").help("effettua il logout");
-        commandParser.addParser("create").help("");
-        // Continue with the other client commands...
+                .build()
+                .defaultHelp(true)
+                .description("TURING distributed program client");
+        argpars.addArgument("-t", "--tcp-command-port").help("TCP commands port").type(Integer.class);
+        argpars.addArgument("-u", "--udp-multicast-port").help("UDP multicast port").type(Integer.class);
+        argpars.addArgument("-r", "--rmi-port").help("RMI communication port").type(Integer.class);
+        argpars.addArgument("-c", "--config-file").help("server configuration file path").type(String.class);
+
+        Namespace ns = null;
+
+        try {
+            ns = argpars.parseArgs(args);
+        }
+        catch(ArgumentParserException ex) {
+            argpars.printHelp();
+            System.exit(1);
+        }
+        Client client = new Client();
+        client.setup(ns);
     }
 }
